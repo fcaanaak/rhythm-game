5ccf8ed148dcd19b246b467a8fd94a3f62eac091
fcaanaak <fcanak05@gmail.com>
2025-08-16 00:21:02 -0700

    Added TrackLine fetching and planned out level editor states

--- rotating-rhythm-game/Level Editor/track_line_manager.gd
+++ rotating-rhythm-game/Level Editor/track_line_manager.gd
@@ -13,7 +13,22 @@ func display_lines(spacing:int,number_of_lines:int)->void:
 func clear_lines()->void:
 	get_tree().call_group("track_lines","queue_free")
 
-
+func get_track_line(beat:float)->TrackLine:
+	"""
+	Retrieve a TrackLine object given its beat
+	
+	Params:
+		beat: The beat of the TrackLine we are looking for
+	
+	Returns:
+		A matching TrackLine object if found else null
+	"""
+	for track_line in get_tree().get_nodes_in_group("track_lines"):
+		if track_line.local_beat_num == beat:
+			return track_line
+			
+	return null
+	
 func _on_level_editor_lines_redrawn(new_beat:float,bar_lines:int)->void:
 	var sample_line = TrackLine.new(0,false)
 	sample_line.bar_num = 0
